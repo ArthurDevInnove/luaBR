73afc80c6cc774ff3d5f901c2c804e6ce19b11d8
arthur <arthur.o.f.miranda2011@gmail.com>
2025-01-29 00:23:34 -0300

    Features: Creating user schemas, auth routers, fake user model and starting a frontend.

--- src/api/main.py
+++ src/api/main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from .routers.auth import router
 
 app = FastAPI()
+app.include_router(router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite todas as origens
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos os métodos (GET, POST, etc)
+    allow_headers=["*"],  # Permite todos os cabeçalhos
+)
 
 @app.get('/')
 async def read_root():
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+      "./src/frontend/html/**/*.{html,js,ts,jsx,tsx}", // Isso inclui todos os arquivos HTML, JS, TS, JSX e TSX dentro de src/frontend/html
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
